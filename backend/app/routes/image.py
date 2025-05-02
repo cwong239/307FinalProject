987609645d2ea4c02b9bf5fd4754c4a95cfd31ac
Vladimir Gupenko <vgupenko@calpoly.edu>
2025-05-02 14:04:49 -0700

    Added image processing files to project.

---
+++ backend/app/routes/image.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+from ..controllers.image import process_image_controller
+
+image_bp = Blueprint('image_bp', __name__)
+
+@image_bp.route('/image', methods=['POST'])
+def image_route():
+    return process_image_controller()
