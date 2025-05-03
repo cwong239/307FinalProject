8baabd4a42ed5b444433d9271758379ca11f9a37
Vladimir Gupenko <vgupenko@calpoly.edu>
2025-05-02 20:02:39 -0700

    Added gamma djustment.

--- backend/app/routes/image.py
+++ backend/app/routes/image.py
@@ -3,9 +3,7 @@ from ..controllers.image import process_image_controller
 
 image_bp = Blueprint('image_bp', __name__)
 
-@image_bp.route('/image', methods=['POST'])
+@image_bp.route("/image", methods=["POST"])
 def image_route():
-
-    #print("image endpoint called.")
 
     return process_image_controller()
