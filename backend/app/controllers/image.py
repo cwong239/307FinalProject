0ed63641c2448ba294fe963e53fbeca2e672e4ea
Vladimir Gupenko <vgupenko@calpoly.edu>
2025-05-05 15:36:37 -0700

    Fixed file size-checking bug and removed commented-out code.

--- backend/app/controllers/image.py
+++ backend/app/controllers/image.py
@@ -8,8 +8,6 @@ from app.utils.response import *
 
 def process_image_controller():
 
-    #print("process_image_controller() called.")
-
     # Retrieve brightness and contrast from query parameters
     brightness = request.args.get("brightness", default=0, type=float)
     contrast = request.args.get("contrast", default=1.0, type=float)
@@ -19,32 +17,25 @@ def process_image_controller():
 
     # Check if an image file is provided
     if "image" not in request.files:
-        #return jsonify({"error": "No image file provided."}), 400
         return HTTPResponse(400).error("No image file provided.").send()
 
     # Read the image file as a numpy array
     uploaded_file = request.files["image"]
     file_bytes = np.frombuffer(uploaded_file.read(), np.uint8)
-    
-    file_size = uploaded_file.content_length
-    #print("File size: ")
-    #print(file_size)
+    file_size = len(file_bytes)
     
     if file_size <= 0:
-	    return HTTPResponse(400).error("Invalid image file.").send()
+	    return HTTPResponse(400).error("Image file is empty.").send()
     
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
     if image is None:
-        #return jsonify({"error": "Invalid image file."}), 400
         return HTTPResponse(400).error("Invalid image file.").send()
 
     try:
         encoded_image_bytes = process_image(image, brightness, contrast,
 											gamma, opacity, remove_bg)
     except Exception as e:
-        #print("Failed to process image.")
-        #return jsonify({"error": str(e)}), 500
         return HTTPResponse(500).error(str(e)).send()
 
     # Return the processed image as a downloadable JPEG file
