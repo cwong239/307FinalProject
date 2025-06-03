89b0faee5d0708af1cac09d33f138454378f4fad
ShreymShah <shreysms04@gmail.com>
2025-06-02 17:16:16 -0800

    fixed npm run lint errors

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import Navbar from "./Navbar";
@@ -70,7 +70,7 @@ function LoginPage() {
         </form>
         {error && <p style={{ color: "red", marginTop: "1rem" }}>{error}</p>}
         <p className="auth-switch">
-          Don't have an account? <a href="/signup">Sign Up</a>
+          Don&#39;t have an account? <a href="/signup">Sign Up</a>
         </p>
       </motion.div>
       <footer className="footer">
