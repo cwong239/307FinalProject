63eb371e54bdb7c3f2c80de9bf1cc1420c40d92d
zih <74742492+zihoffman7@users.noreply.github.com>
2025-06-03 17:32:15 -0700

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,8 @@
 from app import create_app
 import os
-print(f"Port env {os.environ.get('PORT')}")
 
 app = create_app()
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
-  app.run(debug=True, host="0.0.0.0", port=port)
+  app.run(debug=False, host="0.0.0.0", port=port)
